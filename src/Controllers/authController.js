e19e2a6665beb23f5916d08c065490121e38c890
Thanh Ngo <yenthanh5697@gmail.com>
2022-10-14 10:57:45 +0700

    update

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -39,16 +39,16 @@ const authController = {
 				return res.status(403).json("password wrong");
 			}
 			if (user && passwordValid) {
-				// const accessToken = jwt.sign(
-				// 	{
-				// 		id: user?.id,
-				// 		admin: user?.admin,
-				// 	},
-				// 	process.env.JWT_TOKEN_NAME,
-				// 	{ expiresIn: "20d" }
-				// );
-				// const { password, ...other } = user?._doc;
-				return res.status(200).json("Login Successfully");
+				const accessToken = jwt.sign(
+					{
+						id: user?._id,
+						admin: user?.admin,
+					},
+					process.env.JWT_TOKEN_NAME,
+					{ expiresIn: "20d" }
+				);
+				const { password, ...other } = user?._doc;
+				return res.status(200).json(other, accessToken);
 			}
 		} catch (error) {
 			return res.status(500).json("Request Error Wrong");
